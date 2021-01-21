ccd533519da4590e72beabf9919a6ac087b9abe3
Shivam-Dubey10 <75995511+Shivam-Dubey10@users.noreply.github.com>
2021-01-21 14:02:19 +0530

    Add files via upload

---
+++ day37/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    List<String> powerSet(List<Integer> set) {
+        List<String> powerset = new ArrayList<>();
+        if (set == null || set.isEmpty()) {
+            return powerset;
+        }
+        int numberOfSets = (int) Math.pow(2, set.size());
+        for (int index = 0; index < numberOfSets; index++) {
+            List<String> currentSet = new ArrayList<>();
+            for (int count = 0; count < set.size(); count++) {
+                if ((index & (1 << count)) > 0) {
+                    currentSet.add(Integer.toString(set.get(count)));
+                }
+            }
+            powerset.add(String.join(",", currentSet));
+        }
+        powerset.sort(String::compareTo);
+        return powerset;
+    }
+}
